d4f3000371120cb26dadfe4a8eff4e45f51f06d9
cathal-rafferty <123083053+cathal-rafferty@users.noreply.github.com>
2023-01-24 16:26:46 +0000

    Add files via upload

--- 01_single_responsibility/src/Board.java
+++ 01_single_responsibility/src/Board.java
@@ -11,35 +11,17 @@ public class Board {
         }
     }
 
-    public List<String> firstRow() {
-        List<String> firstRow = new ArrayList<>();
-        firstRow.add(this.spots.get(0));
-        firstRow.add(this.spots.get(1));
-        firstRow.add(this.spots.get(2));
-        return firstRow;
-    }
-
-    public List<String> secondRow() {
-        List<String> secondRow = new ArrayList<>();
-        secondRow.add(this.spots.get(3));
-        secondRow.add(this.spots.get(4));
-        secondRow.add(this.spots.get(5));
-        return secondRow;
-    }
-
-    public List<String> thirdRow() {
-        List<String> thirdRow = new ArrayList<>();
-        thirdRow.add(this.spots.get(6));
-        thirdRow.add(this.spots.get(7));
-        thirdRow.add(this.spots.get(8));
-        return thirdRow;
-    }
-
-    // Poor code — can you improve this?
-    public void display() {
-        String formattedFirstRow = this.spots.get(0) + " | " + this.spots.get(1) + " | " + this.spots.get(2) + "\n"
-            + this.spots.get(3) + " | " + this.spots.get(4) + " | " + this.spots.get(5) + "\n"
-            + this.spots.get(6) + " | " + this.spots.get(7) + " | " + this.spots.get(8);
-        System.out.print(formattedFirstRow);
-    }
 }
+
+
+class Display {
+  public void display(List<String> spots){
+    String formattedSpots = spots.get(0) + " | " + spots.get(1) + " | " + spots.get(2) + "\n"
+      + spots.get(3) + " | " + spots.get(4) + " | " + spots.get(5) + "\n"
+            + spots.get(6) + " | " + spots.get(7) + " | " + spots.get(8);
+        System.out.print(formattedSpots);
+  }
+}
+
+
+    
